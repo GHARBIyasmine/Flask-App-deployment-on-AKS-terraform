7c6dae2a4191c95b8b50527e2aaeabffe8dcc501
GHARBIyasmine <yasmine.gharbi@insat.ucar.tn>
2024-12-07 16:23:30 +0100

    added index.html + style.css

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
-
+    name = request.args.get('name', 'Geek')  # Default to 'Geek' if no name is provided
+    return render_template('index.html', name=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
