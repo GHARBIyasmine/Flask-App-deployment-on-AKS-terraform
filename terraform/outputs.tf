3f11221696c34ed9d852642b94e0365ff53adba4
GHARBIyasmine <yasmine.gharbi@insat.ucar.tn>
2024-12-08 20:08:09 +0100

    update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,7 @@
-# Outputs
 output "resource_group_name" {
-  value = azurerm_resource_group.rg[0].name
+  value = length(azurerm_resource_group.rg) > 0 ? azurerm_resource_group.rg[0].name : data.azurerm_resource_group.existing_rg.name
 }
 
 output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks[0].name
+  value = length(azurerm_kubernetes_cluster.aks) > 0 ? azurerm_kubernetes_cluster.aks[0].name : data.azurerm_kubernetes_cluster.existing_aks.name
 }
